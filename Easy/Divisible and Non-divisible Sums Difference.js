a11c04d1de94a3a83635b84e52301625bbc2da40
bola-nabil <bolanabil404@gmail.com>
2023-10-31 11:21:55 +0200

    [JavaScript] Solution

---
+++ Easy/Divisible and Non-divisible Sums Difference.js
@@ -0,0 +1,10 @@
+var differenceOfSums = function(n, m) {
+    let num1 = 0, num2 = 0;
+
+    for(let i=1; i<=n; i++) {
+        if(i % m !== 0) num1+=i;
+        if(i % m === 0) num2+=i;
+    }
+
+    return(num1 - num2);
+};
