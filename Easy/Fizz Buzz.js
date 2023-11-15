3df74db8bca9c0880eac3914f253734a3f197077
bola-nabil <bolanabil404@gmail.com>
2023-11-15 17:56:02 +0200

    JavaScript Solution

---
+++ Easy/Fizz Buzz.js
@@ -0,0 +1,11 @@
+var fizzBuzz = function(n) {
+    let result = [];
+
+    for(let i=1; i<=n; i++) {
+        if(i % 3 === 0 && i % 5 === 0) result.push('FizzBuzz');
+        else if(i % 3 === 0) result.push('Fizz');
+        else if(i % 5 === 0) result.push('Buzz');
+        else result.push(`${i}`);
+    }  
+    return result.splice('');
+};
