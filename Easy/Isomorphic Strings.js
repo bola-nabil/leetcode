a2cdcdeed4fded07f2367e9d8b50d3006acff6e9
bola-nabil <bolanabil404@gmail.com>
2023-12-24 12:10:27 +0200

    JavaScript Solution

---
+++ Easy/Isomorphic Strings.js
@@ -0,0 +1,7 @@
+var isIsomorphic = function(s, t) {
+    for (let i=0; i<s.length; i++) {
+        if (s.indexOf(s[i], i + 1) !== t.indexOf(t[i], i + 1))
+            return false;
+    }
+    return true;
+};
