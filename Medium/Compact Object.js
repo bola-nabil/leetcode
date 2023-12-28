45f362ad6c7271bd092c96191af81e8d88fad95e
bola-nabil <bolanabil404@gmail.com>
2023-12-28 13:41:10 +0200

    JS Solution

---
+++ Medium/Compact Object.js
@@ -0,0 +1,13 @@
+var compactObject = function(obj) {
+    if (obj === null) return null;
+    if (Array.isArray(obj)) return obj.filter(Boolean).map(compactObject);
+    if (typeof obj !== "object") return obj;
+
+    const compacted = {};
+    for (const key in obj) {
+        let value = compactObject(obj[key]);
+        if (value) compacted[key] = value;
+    }
+
+    return compacted;
+};
