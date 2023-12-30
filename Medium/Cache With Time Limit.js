03035f255b5093955604f81eb458595f69bd2919
bola-nabil <bolanabil404@gmail.com>
2023-12-30 12:59:22 +0200

    JS Solution

---
+++ Medium/Cache With Time Limit.js
@@ -0,0 +1,21 @@
+var TimeLimitedCache = function() {
+    this.cache = new Map();
+};
+
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    let found = this.cache.has(key);
+    if(found) clearTimeout(this.cache.get(key).ref);
+    this.cache.set(key, {
+        value,
+        ref: setTimeout(() => this.cache.delete(key), duration)
+    });
+    return found;
+};
+
+TimeLimitedCache.prototype.get = function(key) {
+    return this.cache.has(key) ? this.cache.get(key).value: -1;
+};
+
+TimeLimitedCache.prototype.count = function() {
+    return this.cache.size;
+};
