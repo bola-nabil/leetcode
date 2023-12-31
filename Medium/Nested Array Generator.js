81d53d4305e4fb78fcdd5cbbedf150f6249e1549
bola-nabil <bolanabil404@gmail.com>
2023-12-31 13:30:37 +0200

    JS Solution

---
+++ Medium/Nested Array Generator.js
@@ -0,0 +1,8 @@
+var inorderTraversal = function*(arr) {
+    for(const element of arr) {
+        if(Array.isArray(element))
+            yield* inorderTraversal(element);
+        else
+            yield element;
+    }
+};
