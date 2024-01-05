7608d82b719b39142e40e35ee1698e17d25867d1
bola-nabil <bolanabil404@gmail.com>
2024-01-05 22:51:43 +0200

    JS Solution

---
+++ Medium/300. Longest Increasing Subsequence.js
@@ -0,0 +1,16 @@
+var lengthOfLIS = function(nums) {
+    if (!nums || nums.length === 0)
+        return 0;
+
+    const n = nums.length;
+    const dp = new Array(n).fill(1);
+
+    for (let i = 1; i < n; ++i) {
+        for (let j = 0; j < i; ++j) {
+            if (nums[i] > nums[j])
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+    }
+
+    return Math.max(...dp);
+};
