372e29657805adfa5b9d286fef47fe04300fe3bc
bola-nabil <bolanabil404@gmail.com>
2024-01-08 13:26:53 +0200

    JS Solution

---
+++ Hard/1235. Maximum Profit in Job Scheduling.js
@@ -0,0 +1,33 @@
+var jobScheduling = function(startTime, endTime, profit) {
+    const numJobs = profit.length;
+    const jobs = new Array(numJobs);
+
+    for (let i = 0; i < numJobs; ++i)
+        jobs[i] = [endTime[i], startTime[i], profit[i]];
+
+    jobs.sort((a, b) => a[0] - b[0]);
+    const dp = new Array(numJobs + 1).fill(0);
+
+    for (let i = 0; i < numJobs; ++i) {
+        const [endTime, startTime, profit] = jobs[i];
+        const latestNonConflictJobIndex = upperBound(jobs, i, startTime);
+        dp[i + 1] = Math.max(dp[i], dp[latestNonConflictJobIndex] + profit);
+    }
+
+    return dp[numJobs];
+};
+
+function upperBound(jobs, endIndex, targetTime) {
+    let low = 0;
+    let high = endIndex;
+
+    while (low < high) {
+        const mid = Math.floor((low + high) / 2);
+        if (jobs[mid][0] <= targetTime) {
+            low = mid + 1;
+        } else
+            high = mid;
+    }
+
+    return low;
+}
