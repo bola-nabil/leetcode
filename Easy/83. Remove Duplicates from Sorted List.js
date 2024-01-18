53e79dfe9cd95487b7226e5e7212466fb1c66b05
bola-nabil <bolanabil404@gmail.com>
2024-01-18 11:41:38 +0200

    JS Solution

---
+++ Easy/83. Remove Duplicates from Sorted List.js
@@ -0,0 +1,10 @@
+var deleteDuplicates = function(head) {
+    let cur=head;
+    while(cur && cur.next){
+        if(cur.val==cur.next.val)
+            cur.next=cur.next.next;
+        else
+            cur=cur.next;
+    }
+    return head;
+};
