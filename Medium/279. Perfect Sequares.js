4838904ea7b9c2be79d347a2262d81cc25aeb793
bola-nabil <bolanabil404@gmail.com>
2024-02-08 15:18:33 +0200

    JS Solution

---
+++ Medium/279. Perfect Sequares.js
@@ -0,0 +1,12 @@
+var numSquares = function(n) {
+    let dp = new Array(n + 1).fill(Infinity);
+    dp[0] = 0;
+    for (let i = 1; i <= n; ++i) {
+        let min_val = Infinity;
+        for (let j = 1; j * j <= i; ++j) {
+            min_val = Math.min(min_val, dp[i - j * j] + 1);
+        }
+        dp[i] = min_val;
+    }
+    return dp[n];
+};
