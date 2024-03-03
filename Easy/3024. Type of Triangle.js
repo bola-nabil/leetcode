ace79d7115e1279c669d7e73c7f5d55f90ce444d
bola-nabil <bolanabil404@gmail.com>
2024-03-03 06:11:12 +0200

    JS Solution

---
+++ Easy/3024. Type of Triangle.js
@@ -0,0 +1,12 @@
+var triangleType = function (nums) {
+  nums.sort((a, b) => a - b);
+  if (nums[0] + nums[1] <= nums[2]) {
+    return "none";
+  } else if (nums[0] === nums[1] && nums[1] === nums[2]) {
+    return "equilateral";
+  } else if (nums[0] === nums[1] || nums[1] === nums[2]) {
+    return "isosceles";
+  } else {
+    return "scalene";
+  }
+};
