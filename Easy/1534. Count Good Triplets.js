83fdef1da46e74a7b15aae069f3191520d84ad8f
bola-nabil <bolanabil404@gmail.com>
2024-03-04 12:29:58 +0200

    JS Solution

---
+++ Easy/1534. Count Good Triplets.js
@@ -0,0 +1,17 @@
+var countGoodTriplets = function (arr, a, b, c) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      for (let k = j + 1; k < arr.length; k++) {
+        if (
+          Math.abs(arr[i] - arr[j]) <= a &&
+          Math.abs(arr[j] - arr[k]) <= b &&
+          Math.abs(arr[i] - arr[k]) <= c
+        )
+          count++;
+      }
+    }
+  }
+
+  return count;
+};
