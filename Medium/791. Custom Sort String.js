6c6a40745666cb81af1f1b58f4216a6f149db7bf
bola-nabil <bolanabil404@gmail.com>
2024-03-11 11:02:03 +0200

    JS Solution

---
+++ Medium/791. Custom Sort String.js
@@ -0,0 +1,17 @@
+var customSortString = function(order, s) {
+    let result = '';
+    let mp = new Map();
+    for (let char of s) {
+        mp.set(char, (mp.get(char) || 0) + 1);
+    }
+    for (let char of order) {
+        if (mp.has(char)) {
+            result += char.repeat(mp.get(char));
+            mp.delete(char);
+        }
+    }
+    for (let [char, count] of mp.entries()) {
+        result += char.repeat(count);
+    }
+    return result;
+};
