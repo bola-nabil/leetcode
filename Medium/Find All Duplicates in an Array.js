dd6ee4096f0bd7f83c62e16a8df8e95dd052ae5d
bola-nabil <bolanabil404@gmail.com>
2024-03-25 12:43:28 +0200

    JS Solution

---
+++ Medium/Find All Duplicates in an Array.js
@@ -0,0 +1,12 @@
+var findDuplicates = function(nums) {
+    const ans = [];
+    const n = nums.length;
+    for (let i = 0; i < n; i++) {
+        const x = Math.abs(nums[i]);
+        if (nums[x - 1] < 0) {
+            ans.push(x);
+        }
+        nums[x - 1] *= -1;
+    }
+    return ans;
+};
