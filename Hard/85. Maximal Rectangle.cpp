81d87ecf2187dd866a219c8a72cf039721b13522
bola-nabil <bolanabil404@gmail.com>
2024-04-13 11:00:46 +0200

    CPP Solution

---
+++ Hard/85. Maximal Rectangle.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+int largestRectangleArea(vector < int > & histo) {
+    stack <int> st;
+    int maxA = 0;
+    int n = histo.size();
+    for (int i = 0; i <= n; i++) {
+        while (!st.empty() && (i == n || histo[st.top()] >= histo[i])) {
+            int height = histo[st.top()];
+            st.pop();
+            int width;
+            if (st.empty())
+                width = i;
+            else
+                width = i - st.top() - 1;
+            maxA = max(maxA, width * height);
+        }
+        st.push(i);
+    }
+    return maxA;
+}
+int maximalAreaOfSubMatrixOfAll1(vector<vector<char>> &mat, int n, int m) {
+   
+    int maxArea = 0;
+    vector<int> height(m, 0);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (mat[i][j] == '1') height[j]++;
+            else height[j] = 0;
+        }
+        int area = largestRectangleArea(height);
+        maxArea = max(maxArea, area);
+    }
+    return maxArea;
+}
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        return maximalAreaOfSubMatrixOfAll1(matrix,matrix.size(),matrix[0].size());
+    }
+};
