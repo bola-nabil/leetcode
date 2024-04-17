d399195cdbc2cb12755ca11b6439051876dfc80c
bola-nabil <bolanabil404@gmail.com>
2024-04-17 09:51:58 +0200

    JS Solution

---
+++ Medium/988. Smallest String Starting From Leaf.js
@@ -0,0 +1,19 @@
+var smallestFromLeaf = function(root) {
+    let prevMax;
+    const alphabhets = 'abcdefghijklmnopqrstuvwxyz';
+    function smallest(root, prevStr) {
+        if (!root) return;
+         const str = `${alphabhets[root.val]}${prevStr}`;
+
+        if (!root.left && !root.right)
+            prevMax = (!prevMax || prevMax > str) ? str : prevMax;
+
+        if (root.left)
+            smallest(root.left, str);
+
+        if (root.right)
+            smallest(root.right, str);
+    }
+    smallest(root, '');
+    return prevMax || '';
+};
