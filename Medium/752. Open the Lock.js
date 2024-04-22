0edde93611fd9f1b6d0a959146e4110ea4b95a67
bola-nabil <bolanabil404@gmail.com>
2024-04-22 07:37:14 +0200

    JS Solution

---
+++ Medium/752. Open the Lock.js
@@ -0,0 +1,26 @@
+var openLock = function(deadends, target) {
+    if (target === "0000") return 0
+    let queue = [0], seen = new Uint8Array(10000)
+    for(let d of deadends)
+        seen[~~d] = 1
+    target = ~~target
+    if (seen[0]) return -1
+    for (let turns = 1; queue.length; turns++) {
+        let qlen = queue.length
+        for (let i = 0; i < qlen; i++) {
+            let curr = queue.shift()
+            for (let j = 1; j < 10000; j *= 10) {
+                let mask = ~~(curr % (j * 10) / j),
+                    masked = curr - (mask * j)
+                for (let k = 1; k < 10; k += 8) {
+                    let next = masked + (mask + k) % 10 * j
+                    if (seen[next]) continue
+                    if (next === target) return turns
+                    seen[next] = 1
+                    queue.push(next)
+                }
+            }
+        }
+    }
+    return -1
+};
