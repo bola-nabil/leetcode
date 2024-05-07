98258166efc844feef020e03a19ebd1bf3b42f7e
bola-nabil <bolanabil404@gmail.com>
2024-05-07 11:17:07 +0300

    Daily Solution With CPP

---
+++ Medium/2816. Double a Number Represented as a Linked List.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    ListNode* doubleIt(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+
+        while (curr != nullptr) {
+            int twiceOfVal = curr->val * 2;
+
+            if (twiceOfVal < 10) {
+                curr->val = twiceOfVal;
+            } 
+
+            else if (prev != nullptr) {
+                curr->val = twiceOfVal % 10;
+                prev->val = prev->val + 1;
+            } 
+            else {
+                head = new ListNode(1, curr);
+                curr->val = twiceOfVal % 10;
+            }
+            prev = curr;
+            curr = curr->next;
+        }
+        return head;
+    }
+};
