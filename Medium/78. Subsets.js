7b16c096f6e830337e7df1288833bf1c5ee2dc9c
bola-nabil <bolanabil404@gmail.com>
2024-05-21 19:58:53 +0300

    Daily Solution With JS

---
+++ Medium/78. Subsets.js
@@ -0,0 +1,13 @@
+var subsets = function(nums) {
+    const powerset = [];
+	generatePowerset([], 0);
+
+	function generatePowerset(path, index) {
+		powerset.push(path);
+		for (let i = index; i < nums.length; i++) {
+			generatePowerset([...path, nums[i]], i + 1);
+		}
+	}
+
+	return powerset;
+};
